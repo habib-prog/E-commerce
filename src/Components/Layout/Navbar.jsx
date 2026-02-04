f880b203753f86a98484d25de2c88b59e36e0e55
habib-prog <xavierjames701@gmail.com>
2026-02-04 23:15:54 +0600

    Nav section responsive

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Children } from "react";
 import { Link } from "react-router";
 import { CiSearch } from "react-icons/ci";
-import { FaRegUser } from "react-icons/fa";
+import { FaBars, FaRegUser } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 import { BiChevronDown } from "react-icons/bi";
 
@@ -37,10 +37,14 @@ const Navbar = () => {
     <header>
       <nav className="py-5">
         <div className="container flex justify-between items-center ">
-          <Link to="/">
-            <img src="/logo.png" alt="logo" />
+          <button className="sm:hidden">
+            <FaBars className="text-primary text-2xl " />
+          </button>
+          <Link to="/" className="inline-block sm:w-auto w-36">
+            <img className="w-full" src="/logo.png" alt="logo" />
           </Link>
-          <div className="search flex items-center p-2 gap-2  rounded-lg bg-[#F3F9FB] w-full max-w-126.75">
+          {/* Desktop Navigation Bar Started */}
+          <div className="search hidden sm:flex items-center p-2 gap-2  rounded-lg bg-[#F3F9FB] w-full max-w-126.75">
             <CiSearch />
             <input
               className="outline-none flex-1  "
@@ -48,27 +52,40 @@ const Navbar = () => {
               placeholder="Search essentials, groceries and more..."
             />
           </div>
+          {/* Desktop Navigation Bar Ended */}
           <div className="auth flex items-center gap-10">
             <Link
               to="/signin"
-              className="flex text-base items-center font-bold gap-1.5 text-primary relative after:absolute after:h-full after:w-0.5 after:bg-primary after:top-0 after:-right-5"
+              className=" hidden md:flex text-base items-center font-bold gap-1.5 text-primary relative after:absolute after:h-full after:w-0.5 after:bg-primary after:top-0 after:-right-5"
             >
               <FaRegUser className="text-brand text-2xl" />
               Sign Up/ Sign in
             </Link>
             <Link
               to="cart"
-              className="flex text-base items-center font-bold gap-1.5"
+              className="flex text-base items-center text-primary font-bold gap-1.5"
             >
-              <IoCartOutline className="text-brand text-2xl" />
-              Cart
+              <IoCartOutline className="text-brand  text-2xl" />
+              <span className="hidden sm:block"> Cart</span>
             </Link>
           </div>
         </div>
+        {/* Mobile nav start */}
+        <div className="container flex justify-center items-center sm:hidden">
+          <div className="search mt-4  flex items-center p-2 gap-2  rounded-lg bg-[#F3F9FB] w-full max-w-126.75">
+            <CiSearch />
+            <input
+              className="outline-none flex-1  "
+              type="text"
+              placeholder="Search essentials, groceries and more..."
+            />
+          </div>
+        </div>
+        {/* Mobile nav end */}
       </nav>
       {/* Products category Started */}
-      <div>
-        <div className="container flex gap-2">
+      <div className="p-4 border-third border-y hidden md:block ">
+        <div className=" container flex gap-2">
           {categories.map((item) => (
             <div key={item.title} className="relative group">
               <Link className="hover:bg-brand hover:text-white font-medium bg-third px-3 py-2  rounded-2xl inline-block ">
