98b310bb66a02b1c2b0553a05feca60d5ecf12f0
habib-prog <xavierjames701@gmail.com>
2026-02-11 23:48:52 +0600

    Sidebar done

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -1,12 +1,14 @@
-import React, { Children, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router";
 import { CiSearch } from "react-icons/ci";
 import { FaBars, FaRegUser } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 import { BiChevronDown, BiChevronRight } from "react-icons/bi";
+import { IoIosCloseCircle } from "react-icons/io";
 
 const Navbar = () => {
   const [open, setOpen] = useState("");
+  const [Sidebaropen, setSidebarOpen] = useState(false);
   const categories = [
     //  ["Iphone", "Samsung", "One Plus", "Pixel", "Nothing Phone"]
     {
@@ -153,7 +155,7 @@ const Navbar = () => {
     <header>
       <nav className="py-5">
         <div className="container flex justify-between items-center ">
-          <button className="sm:hidden">
+          <button onClick={() => setSidebarOpen(true)} className="sm:hidden">
             <FaBars className="text-primary text-2xl " />
           </button>
           <Link to="/" className="inline-block sm:w-auto w-36">
@@ -186,7 +188,8 @@ const Navbar = () => {
             </Link>
           </div>
         </div>
-        {/* Mobile nav start */}
+
+        {/* Mobile search start */}
         <div className="container flex justify-center items-center sm:hidden">
           <div className="search mt-4  flex items-center p-2 gap-2  rounded-lg bg-[#F3F9FB] w-full max-w-126.75">
             <CiSearch />
@@ -197,7 +200,7 @@ const Navbar = () => {
             />
           </div>
         </div>
-        {/* Mobile nav end */}
+        {/* Mobile search end */}
       </nav>
       {/* Products category Started */}
       <div className="p-4 border-third border-y hidden md:block ">
@@ -230,15 +233,33 @@ const Navbar = () => {
 
       {/* MOBILE NAVBAR START */}
 
-      <div className="fixed top-0 left-0 bg-primary/30 w-full h-screen">
-        <div className="bg-themey overflow-y-auto w-4/5 sm:w-3/5 h-full p-4">
+      <div
+        className={`fixed top-0 left-0 w-full h-screen z-50 transition-all duration-700 ${
+          Sidebaropen
+            ? "bg-primary/40 visible opacity-100"
+            : "bg-transparent invisible opacity-0"
+        }`}
+        onClick={() => setSidebarOpen(false)}
+      >
+        <div
+          className="bg-themey overflow-y-auto w-4/5 sm:w-3/5 h-full p-4"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="  text-primary pb-3 border-b border-secodary flex justify-between">
+            <h5 className="text-xl text-primary">Menu Sidebar</h5>
+            <button onClick={() => setSidebarOpen(false)} className="text-2xl">
+              <IoIosCloseCircle />
+            </button>
+          </div>
           <ul className=" space-y-4 text-primary font-bold text-base mb-5 pb-4 border-secodary border-b">
             {categories.map((items) => (
               <li key={items.title}>
                 <div className="flex justify-between">
-                  <Link to={items.to}>{items.title}</Link>
+                  <Link onClick={() => setOpen(items.title)} to={items.to}>
+                    {items.title}
+                  </Link>
                   <button
-                    className="text-3xl"
+                    className="text-3xl cursor-pointer"
                     onClick={() => setOpen(items.title)}
                   >
                     <BiChevronRight />
